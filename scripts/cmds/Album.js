c012241fc43bd2ee74964a16cd8c48579af84a35
RAFI-GOAT-BOT-2 <rafibuzz538@gmail.com>
2026-02-05 10:20:25 +0600

    Update Album.js

--- scripts/cmds/Album.js
+++ scripts/cmds/Album.js
@@ -111,7 +111,7 @@ module.exports = {
 
       writer.on("finish", () => {
         message.reply({
-          body: `✦ 𝐀𝐋𝐁𝐔𝐌 𝐃𝐄𝐋𝐈𝐕𝐄𝐑𝐄𝐃 ✦\n💖 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐲 : ${category}\n👑 𝐎𝐰𝐧𝐞𝐫 : 亗🅼🅰ᥫᩣ🅼ᥫᩣ🆄🅽×͜×`,
+          body: `✦ 𝐀𝐋𝐁𝐔𝐌 𝐃𝐄𝐋𝐈𝐕𝐄𝐑𝐄𝐃 ✦\n💖 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐲 : ${category}\n👑 𝐎𝐰𝐧𝐞𝐫 : 𝙍𝙖𝙛𝙞 𝘽𝙝𝙖𝙞✓`,
           attachment: fs.createReadStream(filePath)
         }, () => {
           if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
